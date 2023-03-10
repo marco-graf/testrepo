1f52b7799c1183f059dfaee4fb3067cb6c313b5b
marco-graf <vox123@gmx.ch>
2023-03-10 22:48:36 +0100

    Select columns and filter for specific data

--- my_first_commit.R
+++ my_first_commit.R
@@ -1,2 +1,33 @@
+# Load the tidyverse package 
 library(tidyverse)
-view(mpg)
+
+
+
+# Display the mpg data frame
+View(mpg)
+
+
+
+# Select the columns manufacturer, model, and year.
+# Filter for audi and the year 2008. 
+
+df_1 <- mpg %>% 
+  select(manufacturer, model, year) %>% 
+  filter(manufacturer == "audi" & year == 2008)
+
+# Display the new data frame.
+View(df_1)
+
+
+
+# Select the columns manufacturer, model, and year. 
+# Filter for audi, ford, and jeep and the year 2008. 
+
+df_2 <- mpg %>% 
+  select(manufacturer, model, year) %>% 
+  filter(manufacturer %in% c("audi", "ford", "jeep") & year == 2008)
+
+# Display the new data frame.
+View(df_2)
+
+
